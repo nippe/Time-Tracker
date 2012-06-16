63510f0cefe35483dd28ff58162e969d3cd7398a
Niklas Nihlen <niklas.nihlen@gmail.com>
2012-06-16 09:03:30 +0200

    Added engagement_id to attr accesible

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  attr_accessible :comment, :date, :description, :time_estimate, :time_spent
+  attr_accessible :comment, :date, :description, :time_estimate, :time_spent, :engagement_id
   belongs_to :engagement
 end
